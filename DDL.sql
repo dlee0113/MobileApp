3318c8b3c09912b127d0f0f5911f044f4d479b3f
Dong Lee <dlee0113@gmail.com>
2016-08-03 08:51:10 -0400

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -3,9 +3,9 @@ create database hiberndatedb;
 use hibernatedb;
 
 CREATE TABLE smartphones(
-id INT NOT NULL AUTO_INCREMENT,
-producer VARCHAR(100) NOT NULL,
-model VARCHAR(40) NOT NULL,
-price DOUBLE(16,4),
-PRIMARY KEY ( id )
+  id INT NOT NULL AUTO_INCREMENT,
+  producer VARCHAR(100) NOT NULL,
+  model VARCHAR(40) NOT NULL,
+  price DOUBLE(16,4),
+  PRIMARY KEY ( id )
 );
